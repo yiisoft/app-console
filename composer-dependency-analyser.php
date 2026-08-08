b5cc43a81daabf925c74d9bef64ac52ac74fe8d6
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 14:59:33 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -12,8 +12,11 @@ return (new Configuration())
     ->addPathToScan(__DIR__ . '/src', isDev: false)
     ->addPathToScan(__DIR__ . '/yii', isDev: false)
     ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    ->addPathsToExclude([__DIR__ . '/tests/Support/_generated'])
     ->ignoreErrorsOnPackages(
         ['psr/container', 'yiisoft/config'],
         [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV],
     )
-    ->ignoreErrorsOnPackage('yiisoft/di', [ErrorType::UNUSED_DEPENDENCY]);
+    ->ignoreErrorsOnPackage('yiisoft/di', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('yiisoft/aliases', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('yiisoft/aliases', [ErrorType::UNUSED_DEPENDENCY]);
